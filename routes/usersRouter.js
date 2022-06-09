89b19123e4906ec3397a84462de726d4176c0239
Janette Campbell <campbjp@hotmail.com>
2022-06-09 12:05:35 -0400

    added express-validator to validate email

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const UserModel = require("../models/usersSchema");
+// pulls out the two functions we need from express-validator
+const { check, validationResult } = require("express-validator");
 
 // * Create a Router
 const router = express.Router();
 
-//* Create a new User
-router.post("/", async (req, res) => {
-  const userData = req.body;
+//* Create or Register a new User
+router.post(
+  "/",
+  [
+    check("username", "Username is required from Middleware!").notEmpty(),
+    check("email", "Please use a valid email from Middleware!").isEmail(),
+  ],
+  async (req, res) => {
+    const userData = req.body;
+    const errors = validationResult(req);
 
-  try {
-    const user = await UserModel.create(userData);
-    res.status(201).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json("Bad request!!!!!");
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    try {
+      const user = await UserModel.create(userData);
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json("Bad request!!!!!");
+    }
   }
-});
+);
 
 module.exports = router;
